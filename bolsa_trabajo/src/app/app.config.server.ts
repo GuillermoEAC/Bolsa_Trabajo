9e2a06f450c46aac112b3e33da62095e0249d9d6
GuillermoEAC <guillermochina12@gmail.com>
2025-12-05 00:09:12 -0700

    puro verbo no laja el deploy 7.3

--- bolsa_trabajo/src/app/app.config.server.ts
+++ bolsa_trabajo/src/app/app.config.server.ts
@@ -1,10 +1,27 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
-import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+// import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+// import { provideServerRendering, withRoutes } from '@angular/ssr';
+// import { appConfig } from './app.config';
+// import { serverRoutes } from './app.routes.server';
 
-const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes))],
-};
+// const serverConfig: ApplicationConfig = {
+//   providers: [provideServerRendering(withRoutes(serverRoutes))],
+// };
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+// export const config = mergeApplicationConfig(appConfig, serverConfig);
+
+import { RenderMode, ServerRoute } from '@angular/ssr';
+
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'publicar-vacante/:id',
+    renderMode: RenderMode.Client, // No pre-renderizar
+  },
+  {
+    path: 'ver-candidatos/:id',
+    renderMode: RenderMode.Client, // No pre-renderizar
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender, // Pre-renderizar el resto
+  },
+];
