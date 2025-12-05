d7caf1a48cf23c1e8030e31c6e8ba35ea483d186
GuillermoEAC <guillermochina12@gmail.com>
2025-12-05 00:20:42 -0700

    puro verbo no laja el deploy 7.6

--- bolsa_trabajo/src/app/app.routes.server.ts
+++ bolsa_trabajo/src/app/app.routes.server.ts
@@ -1,11 +1,16 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRoutesConfig } from '@angular/ssr';
-import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
-const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), provideServerRoutesConfig(serverRoutes)],
-};
-
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'publicar-vacante/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'ver-candidatos/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
+  },
+];
