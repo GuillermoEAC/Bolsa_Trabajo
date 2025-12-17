8718bbfece3a2faa92a71c84d2a570843f706a20
GuillermoEAC <guillermochina12@gmail.com>
2025-12-17 00:58:09 -0700

    ojitos 2

--- bolsa_trabajo/backend/config/database.js
+++ bolsa_trabajo/backend/config/database.js
@@ -1,32 +1,24 @@
-// import { createPool } from 'mysql2/promise';
-// import 'dotenv/config';
-
-// export const pool = createPool({
-//   host: process.env.DB_HOST || 'localhost',
-//   user: process.env.DB_USER || 'root',
-//   password: process.env.DB_PASSWORD || '',
-//   database: process.env.DB_NAME || 'PrimerPaso_DB',
-//   port: process.env.DB_PORT || 3306,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
 import { createPool } from 'mysql2/promise';
 import 'dotenv/config';
 
-export const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 4000,
+const dbConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'PrimerPaso_DB',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  // 👇 ESTO ES VITAL PARA TIDB EN RENDER
-  ssl: {
+};
+
+if (process.env.DB_HOST !== 'localhost') {
+  dbConfig.ssl = {
     minVersion: 'TLSv1.2',
     rejectUnauthorized: true,
-  },
-});
+  };
+
+  if (!process.env.DB_PORT) dbConfig.port = 4000;
+}
+
+export const pool = createPool(dbConfig);
